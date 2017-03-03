4d237ceaba409ac7e2ea05d814ef64343531bda4
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-03-03 23:30:24 +0100

    .

---
+++ README.md
@@ -0,0 +1,21 @@
+# nanobus [![stability][0]][1]
+[![npm version][2]][3] [![build status][4]][5] [![test coverage][6]][7]
+[![downloads][8]][9] [![js-standard-style][10]][11]
+
+Tiny message bus
+
+## License
+[MIT](https://tldrlegal.com/license/mit-license)
+
+[0]: https://img.shields.io/badge/stability-experimental-orange.svg?style=flat-square
+[1]: https://nodejs.org/api/documentation.html#documentation_stability_index
+[2]: https://img.shields.io/npm/v/nanobus.svg?style=flat-square
+[3]: https://npmjs.org/package/nanobus
+[4]: https://img.shields.io/travis/yoshuawuyts/nanobus/master.svg?style=flat-square
+[5]: https://travis-ci.org/yoshuawuyts/nanobus
+[6]: https://img.shields.io/codecov/c/github/yoshuawuyts/nanobus/master.svg?style=flat-square
+[7]: https://codecov.io/github/yoshuawuyts/nanobus
+[8]: http://img.shields.io/npm/dm/nanobus.svg?style=flat-square
+[9]: https://npmjs.org/package/nanobus
+[10]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
+[11]: https://github.com/feross/standard
---
+++ index.js
@@ -0,0 +1,95 @@
+var assert = require('assert')
+
+module.exports = Nanobus
+
+function Nanobus () {
+  if (!(this instanceof Nanobus)) return new Nanobus()
+  this._starListeners = []
+  this._starOnces = []
+  this._listeners = {}
+  this._onces = {}
+}
+
+Nanobus.prototype.emit = function (eventName, data) {
+  assert.equal(typeof eventName, 'string', 'nanobus.emit: eventName should be type string')
+
+  if (this._starListeners.length) this._emit(this._startListeners, data)
+  if (this._starOnces.length) {
+    this._emit(this._starOnces, data)
+    this._starOnces = []
+  }
+
+  var listeners = this._listeners[eventName]
+  if (listeners && listeners.length) this._emit(listeners, data)
+}
+
+Nanobus.prototype._emit = function (arr, data) {
+  var length = arr.length
+  for (var i = 0; i < length; i++) {
+    var listener = arr[i]
+    listener(data)
+  }
+}
+
+Nanobus.prototype.on = function (eventName, listener) {
+  assert.equal(typeof eventName, 'string', 'nanobus.on: eventName should be type string')
+  assert.equal(typeof listener, 'function', 'nanobus.on: listener should be type function')
+
+  if (eventName === '*') {
+    this._starListeners.push(listener)
+  } else {
+    if (!this._listeners[eventName]) this._listeners[eventName] = []
+    this._listeners[eventName].push(listener)
+  }
+}
+
+Nanobus.prototype.once = function (eventName, listener) {
+  assert.equal(typeof eventName, 'string', 'nanobus.on: eventName should be type string')
+  assert.equal(typeof listener, 'function', 'nanobus.on: listener should be type function')
+
+  if (eventName === '*') {
+    this._star.push(listener)
+  } else {
+    if (!this._onces[eventName]) this._onces[eventName] = []
+    this._onces[eventName].push(listener)
+  }
+}
+
+Nanobus.prototype.removeListener = function (eventName, listener) {
+  assert.equal(typeof eventName, 'string', 'nanobus.on: eventName should be type string')
+  assert.equal(typeof listener, 'function', 'nanobus.on: listener should be type function')
+
+  if (eventName === '*') {
+    if (remove(this._starOnces)) return
+    if (remove(this._starListeners)) return
+  } else {
+    if (remove(this._listeners[eventName])) return
+    if (remove(this._onces[eventName])) return
+  }
+
+  function remove (arr, listener) {
+    if (!arr) return
+    var index = arr.indexOf(listener)
+    if (index !== -1) {
+      arr.splice(index, 1)
+      return true
+    }
+  }
+}
+
+Nanobus.prototype.removeAllListeners = function (eventName) {
+  if (eventName) {
+    if (eventName === '*') {
+      this._starListeners = []
+      this._starOnces = []
+    } else {
+      this._listeners[eventName] = []
+      this._onces[eventName] = []
+    }
+  } else {
+    this._starListeners = []
+    this._starOnces = []
+    this._listeners = {}
+    this._onces = {}
+  }
+}
