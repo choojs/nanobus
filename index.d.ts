81756b55da64299f1686cb86d900124849f85378
Tyler Murphy <tjames7000@gmail.com>
2019-10-27 16:59:11 -0400

    Updates typescript types so that event names and signatures are specified.
    This is inspired by https://github.com/andywer/typed-emitter/blob/master/index.d.ts, which implements similar typed events for Node's EventEmitter.
    
    Also
    - fixes a typo in the name "_starListeners"
    - types `private _emit`
    - adds a JSDoc comment to explain how to use the constructor
    - fixes the return type for `removeListener`

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,61 @@
-type NanobusListener = (...args: any[]) => void
-type AttachListener = (eventName:string, listener:NanobusListener) => Nanobus
+type EventsConfiguration = { [eventName: string | Symbol]: (...args: any[]) => void }
+type StarListener<Events extends EventsConfiguration> = <EventName extends keyof Events>(eventName: EventName, ...args: Parameters<Events[EventName]>) => void
+type AttachListener<Events extends EventsConfiguration> = <EventName extends (keyof Events) | '*'>(eventName: EventName, listener: EventName extends '*' ? StarListener<Events> : Events[EventName]) => Nanobus<Events>
 
-declare class Nanobus {
-  private _name:string
-  private _startListeners:Array<any>
-  private _listeners:{[key:string]:any}
-  constructor(name?:string)
-  emit:(eventName:string, ...args:any[]) => Nanobus
-  on:AttachListener
-  addListener:AttachListener
-  prependListener:AttachListener
-  once:AttachListener
-  prependOnceListener:AttachListener
-  removeListener:AttachListener
-  removeAllListeners(eventName:string):Nanobus
-  listeners(eventName:string):NanobusListener[]
+declare class Nanobus<Events extends EventsConfiguration> {
+  private _name: string
+  private _starListeners: Array<StarListener<Events>>
+  private _listeners: { [key: keyof Events]: Array<Events[keyof Events]> }
+  private _emit<EventName extends keyof Events>(listeners: Array<Events[infer EventName]>, data: Parameters<Events[EventName]>): void
+  private _emit<EventName extends keyof Events>(listeners: Array<StarListener<Events>>, eventName: EventName, data: Parameters<Events[EventName]>, uuid?: number)
+
+  /**
+   * Allowed events and the arguments that go with them should be specified by passing an interface in which each entry's key defines an event name, and each entry's value defines the signature for a listener for the event.
+   * 
+   * For example:
+   * 
+   * ```
+   * const emitter = new Nanobus<{
+   *   start: (options: Options) => void,
+   *   message: (text: string) => void,
+   *   error: (error: Error) => void,
+   *   result: (part1: number, part2: number) => void,
+   * }>()
+   * ```
+   * 
+   * This will cause emissions and subscriptions to be type-checked.
+   * 
+   * For example, this call would be allowed:
+   * 
+   * ```
+   * emitter.emit('error', new Error('something failed'))
+   * ```
+   * 
+   * This call would be disallowed because the event name isn't allowed:
+   * 
+   * ```
+   * emitter.emit('complete')
+   * ```
+   * 
+   * This call would be disallowed because `part2` must be a number, but isn't:
+   * 
+   * ```
+   * emitter.emit('result', 1, 'text')
+   * ```
+   */
+  constructor(name?: string)
+  emit<EventName extends keyof Events>(eventName: EventName, ...args: Parameters<Events[EventName]>): this
+  on: AttachListener<Events>
+  addListener: AttachListener<Events>
+  prependListener: AttachListener<Events>
+  once: AttachListener<Events>
+  prependOnceListener: AttachListener<Events>
+  removeListener<EventName extends keyof Events, Listener extends Events[EventName]>(eventName: EventName, listener: Listener): true | void
+  removeListener(eventName: '*', listener: StarListener): true | void
+  removeAllListeners<EventName extends keyof Events>(eventName: EventName): this
+  removeAllListeners(eventName: '*'): this
+  listeners<EventName extends keyof Events>(eventName: EventName): Array<Events[EventName]>
+  listeners(eventName: '*'): Array<StarListener<Events>>
 }
 
 export = Nanobus
