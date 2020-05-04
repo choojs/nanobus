2f8e7d7e47bafe20006675054869a00c30ff46e2
Luka Maljic <luka@maljic.com>
2020-05-04 09:30:10 +0200

    Star listener fn sig

--- index.d.ts
+++ index.d.ts
@@ -6,11 +6,11 @@ type Events = { [eventName: string]: Listener }
 
 declare class Nanobus<E extends Events = Events> {
     protected _name: string
-    protected _starListeners: Listener[]
-    protected _listeners: { [K in keyof E]? : E[K][] }
+    protected _starListeners: StarListener<E>[]
+    protected _listeners: { [K in keyof E]?: E[K][] }
     constructor(name?: string)
     emit<K extends keyof E | symbol>(eventName: K, ...args: Parameters<NanobusListener<E, K>>): this
-    protected _emit<K extends keyof E | symbol>(array: NanobusListener<E, K>[], eventName: K, data: Parameters<NanobusListener<E, K>>, performanceTimingId?: string): void
+    protected _emit<K extends keyof E | symbol>(array: StarListener<E>[], eventName: K, data: Parameters<NanobusListener<E, K>>, performanceTimingId?: string): void
     protected _emit<K extends keyof E>(array: E[K][], data: Parameters<E[K]>): void
     protected _emit(array: Listener[], data: any[]): void
     on<K extends keyof E | "*" | symbol>(eventName: K, listener: AnyListener<E, K>): this
@@ -18,9 +18,9 @@ declare class Nanobus<E extends Events = Events> {
     prependListener<K extends keyof E | "*" | symbol>(eventName: K, listener: AnyListener<E, K>): this
     once<K extends keyof E | "*" | symbol>(eventName: K, listener: AnyListener<E, K>): this
     prependOnceListener<K extends keyof E | "*" | symbol>(eventName: K, listener: AnyListener<E, K>): this
-    removeListener<K extends keyof E | "*" | symbol>(eventName: K, listener: NanobusListener<E, K>): true | undefined
+    removeListener<K extends keyof E | "*" | symbol>(eventName: K, listener: AnyListener<E, K>): true | undefined
     removeAllListeners(eventName?: keyof E | "*" | symbol): this
-    listeners<K extends keyof E | "*" | symbol>(eventName: K): NanobusListener<E, K>[]
+    listeners<K extends keyof E | "*" | symbol>(eventName: K): AnyListener<E, K>[]
 }
 
 export = Nanobus
